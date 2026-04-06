131c9da2ae1542494f7a434a28b5f7ca415d068a
sachinhd517 <sachinhd517@gmail.com>
2026-04-06 13:38:13 +0530

    OrderDto Entities

---
+++ OderApi.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+        public int PurchaseQuantity { get; set; }
+        public DateTime OrderedDate { get; set; } = DateTime.UtcNow;
+    }
+}
