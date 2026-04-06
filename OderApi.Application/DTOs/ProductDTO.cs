0e1d9aaa699f3378e3647b64704afe2d250eb209
sachinhd517 <sachinhd517@gmail.com>
2026-04-06 16:55:27 +0530

    create DTOs AppUserDTO, OderDetailsDTO,ProductDTO

---
+++ OderApi.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Application.DTOs
+{
+    public record ProductDTO
+    (
+        int Id,
+        [Required] string Name,
+        [Required, Range(0, int.MaxValue)] int Quantity,
+        [Required, DataType(DataType.Currency)] decimal Price
+    );
+}
