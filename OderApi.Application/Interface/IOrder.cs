46157b653acb860ae52c82b4d774176a55653d9d
sachinhd517 <sachinhd517@gmail.com>
2026-04-08 21:52:10 +0530

    Create IOrder Interface and inheritated form IGenericInterface

---
+++ OderApi.Application/Interface/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.SharedLibrary.Interface;
+using OderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Application.Interface
+{
+    internal interface IOrder : IGenericInterface<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate);
+    }
+}
