14525dc35d3f132fde9af3eedd0125c383d1167b
sachinhd517 <sachinhd517@gmail.com>
2026-04-08 12:49:19 +0530

    Created OrderCOnversion.cs file and add Two methods return single order and return all order list

---
+++ OderApi.Application/DTOs/NewFolder/OrderConversion.cs
@@ -0,0 +1,60 @@
+﻿using OderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Application.DTOs.NewFolder
+{
+    public static class OrderConversion
+    {
+        public static Order ToEntity(OrderDTO order) => new()
+        {
+            Id = order.Id,
+            ClientId = order.ClientId,
+            ProductId = order.ProductId,
+            OrderedDate = order.OrderDate,
+            PurchaseQuantity = order.PurchaseQuantity,
+
+        };
+
+
+        public static (OrderDTO?, IEnumerable<OrderDTO>?) FromEntity(Order? order, IEnumerable<Order?> orders)
+        {
+            // return single order 
+
+            if(order is not null || orders is null)
+            {
+                var singleOrder = new OrderDTO
+                    (
+                        order!.Id,
+                        order.ClientId,
+                        order.ProductId,
+                        order.PurchaseQuantity,
+                        order.OrderedDate
+                    );
+                return (singleOrder, null);
+            }
+
+            // return all orders list 
+
+            if(orders is not null || order is null)
+            {
+                var _orders = orders!.Select
+                    (
+                        o => new OrderDTO
+                        (
+                            o!.Id,
+                            o.ClientId,
+                            o.ProductId,
+                            o.PurchaseQuantity,
+                            o.OrderedDate
+                        )
+                    );
+                return (null, _orders);
+            }
+            return (null, null);
+        }
+    }
+}
