f2525b51cf033d2f2cfe7da0260a2458fbac0a48
sachinhd517 <sachinhd517@gmail.com>
2026-04-11 21:33:56 +0530

    Create DependencyInjection folder add serviceContainer.cs file # Register HttpClient service # Create Dependency Injection # Create Retry Strategy # Use Retry strategy

---
+++ OderApi.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,58 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OderApi.Application.Services;
+using Polly;
+using Polly.Retry;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Application.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+        {
+            // Register HttpClient service
+            // Create Dependency Injection
+
+            services.AddHttpClient<IOrderService, OderService>(options =>
+            {
+                options.BaseAddress = new Uri(config["ProductApi:BaseAddress"]!);
+                options.Timeout = TimeSpan.FromSeconds(1);
+            });
+
+            // Create Retry Strategy
+            var retryStrategy = new RetryStrategyOptions()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<TaskCanceledException>(),
+                BackoffType = DelayBackoffType.Constant,
+                UseJitter = true,
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromMilliseconds(500),
+                OnRetry = args =>
+                {
+                    string message = $"OnRetry, Attempt: {args.AttemptNumber} Outcome {args.Outcome}";
+                    LogException.LogToConsole(message);
+                    LogException.LogToDebugger(message);
+                    return ValueTask.CompletedTask;
+                }
+
+            };
+
+            // Use Retry strategy
+            services.AddResiliencePipeline("my-retry-pipeline", builder =>
+            {
+                builder.AddRetry(retryStrategy);
+            });
+
+
+            return services;
+        }
+    }
+}
