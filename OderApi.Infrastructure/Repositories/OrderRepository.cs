eeb071e4ee32a199c3ab848b6872a39d79363de1
sachinhd517 <sachinhd517@gmail.com>
2026-04-12 09:18:27 +0530

    Create OrderRepository inheriate form IOrder interface

---
+++ OderApi.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,50 @@
+﻿using eCommerce.SharedLibrary.Responses;
+using OderApi.Application.Interface;
+using OderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Infrastructure.Repositories
+{
+    public class OrderRepository() : IOrder
+    {
+        public Task<Response> CreateAsync(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> DeleteAsync(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetByAsync(Expression<Func<Order, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> UpdateAsync(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
