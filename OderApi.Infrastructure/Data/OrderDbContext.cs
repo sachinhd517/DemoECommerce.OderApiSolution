ed90d1efd8c10c8027d2d055e14f096063a6bcef
sachinhd517 <sachinhd517@gmail.com>
2026-04-12 09:28:32 +0530

    Create OrderDbContext file and inherited from DbContext

---
+++ OderApi.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OderApi.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}    
+
