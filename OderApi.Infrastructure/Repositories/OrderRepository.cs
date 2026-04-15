cb30418c9b6a5e72844d6c82c1ccf1353602be0f
sachinhd517 <sachinhd517@gmail.com>
2026-04-15 18:32:05 +0530

    Create OrderRepository class implement IOrder interface or implement the CreateAsync, DeleteAsync, FindByIdAsync, GetAllAsync, GetByAsync, GetOrdersAsync, UpdateAsync

--- OderApi.Infrastructure/Repositories/OrderRepository.cs
+++ OderApi.Infrastructure/Repositories/OrderRepository.cs
@@ -1,6 +1,9 @@
-﻿using eCommerce.SharedLibrary.Responses;
+﻿using eCommerce.SharedLibrary.Logs;
+using eCommerce.SharedLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
 using OderApi.Application.Interface;
 using OderApi.Domain.Entities;
+using OderApi.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,41 +13,139 @@ using System.Threading.Tasks;
 
 namespace OderApi.Infrastructure.Repositories
 {
-    public class OrderRepository() : IOrder
+    public class OrderRepository(OrderDbContext context) : IOrder
     {
-        public Task<Response> CreateAsync(Order entity)
+        public async Task<Response> CreateAsync(Order entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var order = context.Orders.Add(entity).Entity;
+                await context.SaveChangesAsync();
+                return order.Id > 0? new Response(true, "Order placed successfully"): new Response(false, "Error occured while placing orders");
+
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                return new Response(false, "Error occured while placing orders");
+            }
         }
 
-        public Task<Response> DeleteAsync(Order entity)
+        public async Task<Response> DeleteAsync(Order entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var order = await FindByIdAsync(entity.Id);
+                if (order is null)
+                {
+                    return new Response(false, "Order not found");
+                }
+                context.Orders.Remove(order); 
+                await context.SaveChangesAsync(); 
+                return new Response(false, "Order placed successfully");
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                return new Response(false, "Error occured while deleting orders");
+            }
         }
 
-        public Task<Order> FindByIdAsync(int id)
+        public async Task<Order> FindByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var order = await context.Orders.FindAsync(id);
+                return order is not null ? order : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                throw new Exception("Error occured while Retrieving Order 📦");
+            }
         }
 
-        public Task<IEnumerable<Order>> GetAllAsync()
+        public async Task<IEnumerable<Order>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var orders = await context.Orders.AsNoTracking().ToListAsync();
+                return orders is not null ? orders : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                throw new Exception("Error occured while retrieving orders 📦📦📦📦📦📦📦");
+            }
         }
 
-        public Task<Order> GetByAsync(Expression<Func<Order, bool>> predicate)
+        public async Task<Order> GetByAsync(Expression<Func<Order, bool>> predicate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var order = await context.Orders.Where(predicate).FirstOrDefaultAsync();
+                return order is not null ? order : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                throw new Exception("Error occured while retrieving orders 📦📦📦📦📦📦📦" + "Error occured while placing orders");
+            }
         }
 
-        public Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate)
+        public async Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var orders = await context.Orders.Where(predicate).ToListAsync();
+                return orders is not null ? orders : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                throw new Exception("Error occured while retrieving orders 📦📦📦📦📦📦📦" + "Error occured while placing orders");
+            }
         }
 
-        public Task<Response> UpdateAsync(Order entity)
+        public async Task<Response> UpdateAsync(Order entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var order = await FindByIdAsync(entity.Id);
+                if (order is null)
+                {
+                    return new Response(false, "Order not found");
+                }
+                context.Orders.Update(entity);
+                await context.SaveChangesAsync();
+                return new Response(true, "Order updated successfully");
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                return new Response(false, "Error occured while placing orders");
+            }
         }
     }
 }
